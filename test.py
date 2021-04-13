48d7879746f30c334990f472a0ff9cc718f4b586
Eldridge ribeiro <eldridgeriberio@gmail.com>
2021-04-13 21:56:25 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,31 @@
+import tensorflow
+import keras
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences", "age", "goout"]]
+# print(data)
+
+predict = "G3"
+
+X = np.array(data.drop([predict], 1))
+# print(X)
+Y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+print(acc)
+
+# print('Coefficient: \n', linear.coef_)
+# print('Coefficient: \n', linear.intercept_)
+
+predictions = linear.predict(x_test)
